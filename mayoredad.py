d4557a8e5aad6befba38d9dddfb3e4de066050e8
ahruiz <ahruiz0@gmail.com>
2025-06-08 22:59:46 -0700

    se cambia codigo para mejorar salida

--- mayoredad.py
+++ mayoredad.py
@@ -7,13 +7,22 @@ cartelara = {"LA SIRENITA": "A", "EL EXORCISTA": "R", "PINOCHO": "A", "50 SOMBRA
 
 edad = int(input("Introduce tu edad para mostrarte la cartelera: "))
 
-cartmenores = [key for key, value in cartelara.items() if value == "A"]
-cartmayores = [key for key, value in cartelara.items() if value == "R"] + cartmenores
+# cartmenores = [key for key, value in cartelara.items() if value == "A"]
+# cartmayores = [key for key, value in cartelara.items() if value == "R"] + cartmenores
 
+# if edad >= 18:
+#     print(f"nuestra cartelera es: {cartmayores}")
+# else:
+#     print(f"Nuestra cartelera es: {cartmenores} son clasificacion A:para toda la familia")
+#ESTAS DOS SENTENCIAS ANTERIORES ARROJAN EL FORMATO DE DICCIONARIO en el print
 
-if edad >= 18:
-    print(f"nuestra cartelera es: {cartmayores}")
+if edad < 18:
+    for clave, valor in cartelara.items():
+        if valor == "A":
+            print(clave) 
 else:
-    print(f"Nuestra cartelera es: {cartmenores}")
+    for clave,valor in cartelara.items():
+        print(clave)
+#ESTAS SENTENCIAS IF... ARROJAN LOS DATOS SIN EL FORMATO DE DICCIONARIO
 
 print("*" * 80)
---
+++ otro.py
@@ -0,0 +1,23 @@
+print("*" * 80)
+print("Mostrar cartelera de cine segun la edad.....")
+
+cartelara = {"LA SIRENITA": "A", "EL EXORCISTA": "R", "PINOCHO": "A", "50 SOMBRAS DE GRAY": "R"}
+
+edad = int(input("Introduce tu edad para mostrarte la cartelera: "))
+
+#cartmenores = [key for key, value in cartelara.items() if value == "A"]
+#cartmayores = [key for key, value in cartelara.items() if value == "R"] + cartmenores
+# if edad < 18:
+#     for clave, valor in cartelara.items():
+#         if valor == "A":
+#             print(clave) 
+# else:
+#     for clave,valor in cartelara.items():
+#         print(clave)
+
+# if edad >= 18:
+#     print(f"nuestra cartelera es: {cartmayores}")
+# else:
+#     print(f"Nuestra cartelera es: {cartmenores} son clasificacion A:para toda la familia")
+
+print("*" * 80)
