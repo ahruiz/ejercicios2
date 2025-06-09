e4ca6ee0c264c04024d8bf06c804d35977307eee
ahruiz <ahruiz0@gmail.com>
2025-06-08 20:32:27 -0700

    cartelera peliculas por edad

---
+++ mayoredad.py
@@ -0,0 +1,17 @@
+#codigo para generar cartelera de cine segun la edad
+
+print("*" * 80)
+print("Mostrar cartelera de cine segun la edad.....")
+
+cartelara = {"LA SIRENITA": "A", "EL EXORCISTA": "R", "PINOCHO": "A", "50 SOMBRAS DE GRAY": "R"}
+
+edad = int(input("Introduce tu edad para mostrarte la cartelera: "))
+
+cartmenores = [key for key, value in cartelara.items() if value == "A"]
+cartmayores = [key for key, value in cartelara.items() if value == "R"] + cartmenores
+
+
+if edad >= 18:
+    print(f"nuestra cartelera es: {cartmayores}")
+else:
+    print(f"Nuestra cartelera es: {cartmenores}")
