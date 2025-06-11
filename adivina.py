5c6c4ea29d489d5a7b0c046dce43db326562d1fc
ahruiz <ahruiz0@gmail.com>
2025-06-10 21:22:08 -0700

    se agregan comentarios

--- adivina.py
+++ adivina.py
@@ -1,14 +1,18 @@
 #codigo p/generar num aleatoria e intento de adivinar
-import random
+import random    #funcion para crear nums aleatoriamente
 
-print("-" * 80)
-print("Se genera num aleatorio y el usuario intenta adivinarlo")
+print("-" * 80) # linea de separacion en pantalla
+print("Se genera num aleatorio y el usuario intenta adivinarlo")    # explicacion de la funcion del codigo
 
-numAleat = random.randint(1,10)
+numAleat = random.randint(1,10)   # se genera un num aleatorio escondido
 
-miNum = int(input("ingresa el numero que crees que se genero: "))
+miNum = int(input("ingresa el numero que crees que se genero: ")) #pedimos al usuario que adivine
 
 if miNum == numAleat:
+    print(f"Este es el numero escondido: {numAleat}") #mostramos el numero y la felicitacion
     print("¡¡¡FELICIDADES!!! Has ganado un cupon, acertaste el numero oculto")
 else:
+    print(f"Este es el numero escondido: {numAleat}") #mostramos el numero e invitamos a participar de nuevo
     print("MALA SUERTE.....intentalo de nuevo")
+
+print("-" * 80) # linea de separacion en pantalla
