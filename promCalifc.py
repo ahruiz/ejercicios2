264de5445e772b1a957aca4fe90d638645a8a312
ahruiz <ahruiz0@gmail.com>
2025-06-10 22:27:42 -0700

    se agregan comentarios

--- promCalifc.py
+++ promCalifc.py
@@ -1,15 +1,16 @@
 #codigo para calcular el promedio de las calificaciones capturadas por el usuario
 
-print("_" * 80)
-print("Captura tus calificaciones para obtener tu promedio....")
+print("_" * 80) #linea de separacion en pantalla
+print("Captura tus calificaciones para obtener tu promedio....")  #Explicacion
 
-numCalific = int(input("Cuantas calificaciones quieres capturar: "))
-calificaciones = []
+numCalific = int(input("Cuantas calificaciones quieres capturar: ")) #pedimos al usr cuantas calificaciones son
+calificaciones = [] # e genera dicc/lista vacio(a)
 
 for i in range(numCalific):
-    miCalific = int(input(f"Captura tu calificacion {i + 1}: "))
-    calificaciones.append(miCalific)
+    miCalific = int(input(f"Captura tu calificacion {i + 1}: ")) # se genera ciclo en fiuncion del num de califs
+    calificaciones.append(miCalific) #ingresamos cada calificacion al dicc o lista
 
-promedio = round(sum(calificaciones) / numCalific,2)
-print(f"El promedio de las calificaciones capturadas es: {promedio}")
-print("_" * 80)
+promedio = round(sum(calificaciones) / numCalific,2) #determinamos el prom y lo redondeamos a 2 decimales
+print(f"El promedio de las calificaciones capturadas es: {promedio}") #desplegamos resultados
+
+print("_" * 80) #linea de separacion en pantalla
