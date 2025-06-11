dd19c4c5d3fa64068315402781ca7824bdf08ea5
ahruiz <ahruiz0@gmail.com>
2025-06-10 22:30:43 -0700

    se agregan comentarios

--- suma2num.py
+++ suma2num.py
@@ -1,16 +1,17 @@
 #codigo para sumar y mostrar la suma de 2 nums ingresados x el usr
 
-print("-" * 80)
-print("codigo para sumar dos numeros proporcionados por el usuario")
+print("-" * 80) #linea de separacion en pantalla
+print("codigo para sumar dos numeros proporcionados por el usuario") #Explicacion
 
 num1= input("ingresa el primer numero : ")
-num2= input("ingresa el segundo numero: ")
+num2= input("ingresa el segundo numero: ") #pedimos al usr capturar los numeros a sumar
 
 if not num1.isdigit() or not num2.isdigit():
-    print("Teclea numeros enteros.....")
-    exit()
+    print("Teclea numeros enteros.....")   # verificamos que los datos sean correctos
+    exit()                                 # terminamos el proceso si no son correctos
 
-suma = num1 + num2
+suma = num1 + num2 # sumamos
 
-print(f"La suma de los numeros proporcionados es: {suma}")
-print("-" * 80)
+print(f"La suma de los numeros proporcionados es: {suma}") # mostramos resultados
+
+print("-" * 80) #linea de separacion en pantalla
