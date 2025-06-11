df32e6c08712b0adb31dd852ec3f50bcb5feafc1
ahruiz <ahruiz0@gmail.com>
2025-06-10 22:19:45 -0700

    se agregan comentarios

--- palabraMasLarga.py
+++ palabraMasLarga.py
@@ -1,15 +1,16 @@
 #codigo para mostrar la palabra mas larga de un texto
-from colorama import Fore
+from colorama import Fore   # funcion para resaltar texto
 
-print("_" * 80)
-print("Codigo para mostrar la palabra mas larga de un texto dado por el usuario....")
+print("_" * 80) #linea de separacion en pantalla
+print("Codigo para mostrar la palabra mas larga de un texto dado por el usuario....") # explicacion
 
-mitexto = input(Fore.YELLOW + "Ingresa el texto:" + Fore.WHITE)
+mitexto = input(Fore.YELLOW + "Ingresa el texto:" + Fore.WHITE) #pedimos al usr insertar un texto
 
-diccDeMitxt = mitexto.split() #se genera el diccionario
+diccDeMitxt = mitexto.split() #se genera el diccionario agregando cada palabra del texto
 
-palMasL = max(diccDeMitxt, key=len)
-longPalMasL= len(palMasL) 
-print("_" * 80)
+palMasL = max(diccDeMitxt, key=len) #determinamos cual es la palambra mas larga en funcion de su longitud
+longPalMasL= len(palMasL) #sacamos la longitud de la palabra mas larga
+
 print(f"La palabra mas larga es: {Fore.YELLOW} {palMasL} {Fore.YELLOW} con una logitud de {longPalMasL} letras")
-print("_" * 80)
+#mostramos resultados en pantalla
+print("_" * 80)  #linea de separacion en pantalla
