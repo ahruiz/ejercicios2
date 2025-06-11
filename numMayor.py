ce32ba875e23d2633fa221751b5fd8701a2a2674
ahruiz <ahruiz0@gmail.com>
2025-06-10 22:13:57 -0700

    se agregan comentarios

--- numMayor.py
+++ numMayor.py
@@ -1,12 +1,13 @@
 #Codigo para mostrar el num mas alto de una lista
 
-print("_" * 80)
-print("Codigo para determinar el num mayor de una lista....")
+print("_" * 80) #linea de separacion en pantalla
+print("Codigo para determinar el num mayor de una lista....") # explicacion de codigo
 
 misnum = [1,22,3,56,78,25,3,-2,11,99]
 
-numMay =max(misnum)
+numMay =max(misnum) #determinamos cual es el may de la lista
 
 print(f"Mi lista de numeros es: {misnum}")
-print(f"y el num mayor es: {numMay}")
-print("_" * 80)
+print(f"y el num mayor es: {numMay}") # y desplegamos
+
+print("_" * 80) #linea de separacion en pantalla
